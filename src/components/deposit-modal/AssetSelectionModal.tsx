8bb117c8ac399259b3b50c5b6f0203a117134fda
devine200 <devinebabylon@gmail.com>
2025-01-22 04:13:13 +0100

    setting up proper naming

--- src/components/deposit-modal/AssetSelectionModal.tsx
+++ src/components/deposit-modal/AssetSelectionModal.tsx
@@ -1,8 +1,8 @@
 import "./deposit.css";
 import contractConfig from "../../utils/test-config.json";
 import CloseBtn from "../../close-btn.tsx";
-import { AppFeatures, AssetSelectionTransactionType, Deposit, ModalInfo, ModalState } from "../../types.ts";
-import { useCallback, useEffect, useState } from "react";
+import { AppFeatures, AssetSelectionTransactionType, Deposit, ModalState } from "../../types.ts";
+import { useState } from "react";
 import { useSwitchChain } from "wagmi";
 import { config } from "../../wagmi.ts";
 import useGetAssets, { ChainData } from "../../hooks/useGetAssets.tsx";
